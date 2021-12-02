e8ecdc81b869255ff815a23bfb7910a760ffb022
Guillaume Smet <guillaume.smet@gmail.com>
2021-12-02 12:32:22 +0100

    Convert to a Quarkiverse extension

---
+++ README.md
@@ -0,0 +1,14 @@
+# Quarkus - JGit
+
+[![Version](https://img.shields.io/maven-central/v/io.quarkiverse.jgit/quarkus-jgit?logo=apache-maven&style=flat-square)](https://search.maven.org/artifact/io.quarkiverse.jgit/quarkus-jgit)
+
+## Introduction
+
+Quarkus JGit is a Quarkus extension for the [JGit](https://www.eclipse.org/jgit/) library.
+JGit is a library to access to and manipulate Git repositories. 
+
+The main purpose of this extension is to make JGit work in a native executable built with GraalVM/Mandrel.
+
+## Documentation
+
+The documentation for this extension can be found [here](https://quarkiverse.github.io/quarkiverse-docs/quarkus-jgit/dev/index.html).
---
+++ deployment/src/main/java/io/quarkus/jgit/deployment/JGitProcessor.java
@@ -0,0 +1,57 @@
+package io.quarkus.jgit.deployment;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.ExtensionSslNativeSupportBuildItem;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBundleBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
+
+class JGitProcessor {
+
+    private static final String FEATURE = "jgit";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    ExtensionSslNativeSupportBuildItem activateSslNativeSupport() {
+        return new ExtensionSslNativeSupportBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    ReflectiveClassBuildItem reflection() {
+        //Classes that use reflection
+        return new ReflectiveClassBuildItem(true, true,
+                "org.eclipse.jgit.api.MergeCommand$FastForwardMode",
+                "org.eclipse.jgit.api.MergeCommand$FastForwardMode$Merge",
+                "org.eclipse.jgit.internal.JGitText",
+                "org.eclipse.jgit.lib.CoreConfig$AutoCRLF",
+                "org.eclipse.jgit.lib.CoreConfig$CheckStat",
+                "org.eclipse.jgit.lib.CoreConfig$EOL",
+                "org.eclipse.jgit.lib.CoreConfig$EolStreamType",
+                "org.eclipse.jgit.lib.CoreConfig$HideDotFiles",
+                "org.eclipse.jgit.lib.CoreConfig$SymLinks",
+                "org.eclipse.jgit.lib.CoreConfig$LogRefUpdates");
+    }
+
+    @BuildStep
+    List<RuntimeInitializedClassBuildItem> runtimeInitializedClasses() {
+        return Arrays.asList(
+                new RuntimeInitializedClassBuildItem("org.eclipse.jgit.transport.HttpAuthMethod$Digest"),
+                new RuntimeInitializedClassBuildItem("org.eclipse.jgit.lib.GpgSigner"),
+                // The following classes use j.u.Ramdom, so they need to be runtime-initialized
+                new RuntimeInitializedClassBuildItem("org.eclipse.jgit.internal.storage.file.WindowCache"),
+                new RuntimeInitializedClassBuildItem("org.eclipse.jgit.util.FileUtils"));
+    }
+
+    @BuildStep
+    NativeImageResourceBundleBuildItem includeResourceBundle() {
+        return new NativeImageResourceBundleBuildItem("org.eclipse.jgit.internal.JGitText");
+    }
+}
