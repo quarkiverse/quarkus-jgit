5f591b724363961c69e4ddbb87ea62ac0ad815e9
George Gastaldi <gegastaldi@gmail.com>
2024-04-01 12:32:04 -0300

    Sort imports

--- ssh-factories/jsch/deployment/src/main/java/io/quarkiverse/quarkus/jgit/jsch/deployment/JgitJschProcessor.java
+++ ssh-factories/jsch/deployment/src/main/java/io/quarkiverse/quarkus/jgit/jsch/deployment/JgitJschProcessor.java
@@ -1,8 +1,5 @@
 package io.quarkiverse.quarkus.jgit.jsch.deployment;
 
-import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.builditem.FeatureBuildItem;
-
 class JgitJschProcessor {
 
 }
