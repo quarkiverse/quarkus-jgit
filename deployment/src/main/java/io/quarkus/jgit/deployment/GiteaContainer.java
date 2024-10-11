d87cf4d7e8212f699eb018e7019fd03d8b498717
George Gastaldi <gegastaldi@gmail.com>
2024-10-11 20:56:26 -0300

    Show create user cmd in debug only

--- deployment/src/main/java/io/quarkus/jgit/deployment/GiteaContainer.java
+++ deployment/src/main/java/io/quarkus/jgit/deployment/GiteaContainer.java
@@ -62,7 +62,7 @@ class GiteaContainer extends GenericContainer<GiteaContainer> {
                 "--must-change-password=false",
                 "--admin"
         };
-        log.info(String.join(" ", cmd));
+        log.debug(String.join(" ", cmd));
         ExecResult execResult = execInContainer(cmd);
         log.info(execResult.getStdout());
         if (execResult.getExitCode() != 0) {
