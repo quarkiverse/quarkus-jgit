b511fb10807ef0c87df3f966870664aeaf0f8b0e
Phillip Kruger <phillip.kruger@gmail.com>
2025-06-12 10:09:16 +1000

    Use the new logo feature from Dev UI
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- deployment/src/main/java/io/quarkus/jgit/deployment/devui/GiteaDevUIProcessor.java
+++ deployment/src/main/java/io/quarkus/jgit/deployment/devui/GiteaDevUIProcessor.java
@@ -2,13 +2,10 @@ package io.quarkus.jgit.deployment.devui;
 
 import java.util.Optional;
 
-import org.eclipse.jgit.api.Git;
-
 import io.quarkus.deployment.IsDevelopment;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.devui.spi.page.CardPageBuildItem;
-import io.quarkus.devui.spi.page.ExternalPageBuilder;
 import io.quarkus.devui.spi.page.Page;
 import io.quarkus.jgit.deployment.GiteaDevServiceInfoBuildItem;
 
@@ -16,25 +13,15 @@ public class GiteaDevUIProcessor {
 
     @BuildStep(onlyIf = IsDevelopment.class)
     void createCard(Optional<GiteaDevServiceInfoBuildItem> info, BuildProducer<CardPageBuildItem> cardPage) {
-        CardPageBuildItem card = new CardPageBuildItem();
 
         info.ifPresent(i -> {
+            CardPageBuildItem card = new CardPageBuildItem();
             String url = "http://" + i.host() + ":" + i.httpPort();
             card.addPage(Page.externalPageBuilder("Gitea Dashboard")
                     .doNotEmbed()
                     .icon("font-awesome-solid:code-branch")
                     .url(url, url));
+            cardPage.produce(card);
         });
-
-        final ExternalPageBuilder versionPage = Page.externalPageBuilder("JGit Version")
-                .icon("font-awesome-solid:tag")
-                .url("https://www.eclipse.org/jgit/")
-                .doNotEmbed()
-                .staticLabel(Git.class.getPackage().getImplementationVersion());
-
-        card.addPage(versionPage);
-
-        card.setCustomCard("qwc-jgit-card.js");
-        cardPage.produce(card);
     }
 }
